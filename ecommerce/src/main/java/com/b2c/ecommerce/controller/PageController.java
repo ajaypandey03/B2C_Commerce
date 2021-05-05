47d71623b93919fdd62647ac1740632a4b72cbbb
Ajay Pandey <ajay.pandey03@yahoo.com>
2021-05-05 14:50:37 +0530

    Created static resources and homepage with navbar.

---
+++ ecommerce/src/main/java/com/b2c/ecommerce/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.b2c.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("message", "Welcome to My Online Store");
+		return mv;
+	}
+	
+
+}
